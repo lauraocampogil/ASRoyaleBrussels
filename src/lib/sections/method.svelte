2a79864c63fa51dd6c960e2459ca9d685c0069f2
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-07-17 17:22:37 +0200

    id

--- src/lib/sections/method.svelte
+++ src/lib/sections/method.svelte
@@ -41,6 +41,7 @@
 </script>
 
 <section
+	id="methode"
 	bind:this={sectionEl}
 	class="grid-section sm-grid-section bg-background px-5 py-16 sm:px-8 md:px-10 lg:px-12 xl:py-24 3xl:container 3xl:mx-auto"
 >
