f9c7d6e14635d2b4479b696a7852ece0cbae252c
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-07-21 20:13:48 +0200

    Split text to button

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -16,6 +16,23 @@
 	};
 </script>
 
-<a {href} class="text-button rounded px-6 py-3 transition-colors {variants[variant]}">
-	{label}
+<a
+	{href}
+	class="group text-button inline-flex items-center overflow-hidden rounded px-6 py-3 transition-colors {variants[
+		variant
+	]}"
+>
+	{#each label.split('') as char, i}
+		<span class="relative inline-block h-[1em] overflow-hidden align-top leading-none">
+			<span
+				class="flex flex-col leading-none transition-transform ease-out group-hover:-translate-y-1/2"
+				style="transition-duration: 350ms; transition-delay: {i * 20}ms"
+			>
+				<span class="block h-[1em] leading-[1em]">{char === ' ' ? '\u00A0' : char}</span>
+				<span aria-hidden="true" class="block h-[1em] leading-[1em]"
+					>{char === ' ' ? '\u00A0' : char}</span
+				>
+			</span>
+		</span>
+	{/each}
 </a>
